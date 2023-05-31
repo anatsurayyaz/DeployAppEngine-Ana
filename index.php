23728545b620a1f6969301fa7523152f75db3423
anatsurayyaz <117384535+anatsurayyaz@users.noreply.github.com>
2023-06-01 04:52:24 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>profile Ana Tsurayya Zakiya</title>
+  <style>
+    body {
+      font-family: "Quicksand", sans-serif;
+      margin: 0;
+      padding: 0;
+      background-color: #374259;
+    }
+
+    .container {
+      max-width: 700px;
+      margin: 0 auto;
+      padding: 70px;
+      background-color: #5C8984;
+      border-radius: 7px;
+      box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+	  object-position: center;
+    }
+
+    .profile-image {
+      width: 300px;
+      height: 300px;
+      border-radius: 10%;
+      margin-bottom: 0 auto;
+      object-fit: cover;
+      object-position: center;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    }
+
+    h1 {
+      color: #EEE3CB;
+      margin-bottom: 20px;
+      text-align: center;
+    }
+
+    h2 {
+      color: #EEE3CB;
+      margin-bottom: 10px;
+    }
+
+    p {
+      color: #FFFF;
+      line-height: 1.5;
+	  text-align: center;
+    }
+	
+	a {
+      color: #FFFF;
+      line-height: 1.5;
+	  text-align: center;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <h1>My Profile</h1>
+    <img class="profile-image" src="foto-bio.jpg" alt="Profile Photo">
+
+    <h2>Name:</h2>
+    <p>Ana Tsurayya Zakiya</p>
+	
+	<h2>Place and Date of Birth:</h2>
+    <p>Bengkulu, 24 March 2002</p>
+
+    <h2>Address:</h2>
+    <p>Yogyakarta</p>
+
+    <h2>University:</h2>
+    <p>Sunan Kalijaga State Islamic University, Yogyakarta</p>
+	
+	<h2>Linkedin:</h2>
+    <p><a href="https://www.linkedin.com/in/ana-tsurayya-zakiya-509369193/" target="_blank">Ana Tsurayya Zakiya</a></p>
+	
+	<h2>Instagram:</h2>
+    <p><a href="https://www.instagram.com/anatsurayyaz/" target="_blank">anatsurayyaz</a></p>
+	
+	<h2>Email:</h2>
+    <p>anatsurayya03@gmail.com</p>
+  </div>
+</body>
+</html>
